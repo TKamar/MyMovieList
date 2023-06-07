17a6cc5228be106b3d1e498732bd1b9fc02cd96c
Tomer <tomer.kamar@gmail.com>
2023-06-07 18:24:28 +0300

    Add Interceptor okhttp3

--- app/src/main/java/com/example/mymovielist/Services/RetrofitClient.java
+++ app/src/main/java/com/example/mymovielist/Services/RetrofitClient.java
@@ -1,5 +1,11 @@
 package com.example.mymovielist.Services;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,8 +19,23 @@ public class RetrofitClient {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(new OkHttpClient().newBuilder().addInterceptor(newInterceptor()).build())
                     .build();
         }
         return retrofit.create(ApiInterface.class);
+    }
+
+    private static Interceptor newInterceptor() {
+
+        return new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request original =chain.request();
+                HttpUrl httpUrl = original.url().newBuilder().addQueryParameter("api_key","0ca1b003e8550baee324f2d0413eb7ac").build();
+                original = original.newBuilder().url(httpUrl).build();
+                return chain.proceed(original);
+            }
+        };
+
     }
 }
